161ce7e1cd392b2444ccc0b4a32178fb512860f5
Eduard Fomin <googl2.71@gmail.com>
2016-05-20 19:56:58 +0500

    parseXmlFile

---
+++ parseXmlFile.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Bash xml reader
+# Supported tags only, but attributes are also available
+#
+# Example of using:
+# local xmlPath=(recipe composition ingredient);
+# local ingredients=`parseXmlFile $TMP_DIR/filename "${xmlPath[@]}"`;
+#
+
+readXmlDom () {
+    local IFS=\>
+    read -d \< ENTITY CONTENT
+    local ret=$?
+    TAG_NAME=${ENTITY%% *}
+    ATTRIBUTES=${ENTITY#* }
+    return $ret
+}
+
+# $1 - path to file, like /path/to/file
+# $2 - xml-path array, like (Tag InnerTag InnerInnerTag InnerInnerInnerTag)
+# output - string of values separated by commas, like "qwe,asd,zxc"
+parseXmlFile() {
+    local filePath="${1}"
+    shift
+    local path=("${@}")
+
+    local lastIndex=0
+    local current=0
+    local values=''
+    let "lastIndex=${#path[*]}-1"
+    while readXmlDom; do
+        if [[ $TAG_NAME = "${path[current]}" ]]; then
+            if [[ $current -eq lastIndex ]]; then
+                values=$values"\"$CONTENT\","
+            else
+                let "current=$current+1"
+            fi
+        elif [[ $current -eq 0 ]]; then
+            continue;
+        elif [[ $TAG_NAME = "/"${path[current-1]} ]]; then
+            let "current=$current-1"
+        fi
+    done < $filePath
+
+    echo ${values%?}
+}
